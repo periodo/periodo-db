eea3131b307e0d9143cfc3f788174aa6368a766b
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-15 22:57:42 +0200

    geo indexes

--- convert.js
+++ convert.js
@@ -8,25 +8,41 @@ var minimist = require('minimist')
 var argv = minimist(process.argv.slice(2), {
   alias: { d: 'datadir' }
 })
+var geoid = 1
+var geometries = {}
 
 process.stdin
   .pipe(parse(['periodCollections',true,'definitions',true]))
   .pipe(through.obj(function (def, enc, next) {
+    var stream = this
     next = once(next)
     if (!def.spatialCoverage) return next()
     var pending = 1
-    var geo = []
+    var geoids = []
     def.spatialCoverage.forEach(function (sp) {
       if (!sp.label) return
+      if (geometries.hasOwnProperty(sp.label)) {
+        geoids.push(geometries[sp.label])
+        return
+      }
       var hash = shasum(String(sp.label).toLowerCase())
       var parts = [hash.slice(0,2),hash.slice(2,4),hash.slice(4)]
       var file = path.resolve(argv.datadir, parts.join('/') + '.json')
       pending++
       fs.readFile(file, 'utf8', function (err, data) {
-        if (err) return next(err)
+        if (err) {
+          if (--pending === 0) push()
+          return
+        }
         try { var jdata = JSON.parse(data) }
         catch (err) { return next(err) }
-        geo.push(jdata.geometry)
+        geoids.push(geoid)
+        geometries[sp.label] = geoid
+        stream.push(JSON.stringify({
+          type: 'Feature',
+          properties: { id: geoid++ },
+          geometry: jdata.geometry
+        }) + '\n')
         if (--pending === 0) push()
       })
     })
@@ -34,17 +50,19 @@ process.stdin
 
     function push () {
       var ref = {
-        type: geo.length === 1 ? 'Feature' : 'FeatureCollection',
+        type: geoids.length === 1 ? 'Feature' : 'FeatureCollection',
         properties: {
           id: def.id,
           label: def.label,
           localizedLabels: def.localizedLabels,
+          start: def.start,
+          stop: def.stop
         },
       }
-      if (geo.length === 1) ref.geometry = geo[0]
+      if (geoids.length === 1) ref.geometry = geoids[0]
       else {
-        ref.features = geo.map(function (g) {
-          return { type: 'Feature', geometry: g }
+        ref.features = geoids.map(function (gid) {
+          return { type: 'Feature', geometry: gid }
         })
       }
       next(null, JSON.stringify(ref) + '\n')
