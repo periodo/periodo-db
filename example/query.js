1907e68e8c131a04b361a658ce1ba24cf1b7e70d
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-22 15:08:54 +0300

    query by label

---
+++ example/query.js
@@ -0,0 +1,16 @@
+var pquery = require('../')
+var hyperdrive = require('hyperdrive')
+var hyperlog = require('hyperlog')
+var level = require('level')
+var to = require('to2')
+
+var pq = pquery({
+  archive: hyperdrive('pq.data'),
+  db: level('pq.db'),
+  log: hyperlog(level('pq.log'), { valueEncoding: 'json' })
+})
+pq.list(process.argv.slice(2).join(' '))
+  .pipe(to.obj(function (row, enc, next) {
+    console.log(row)
+    next()
+  }))
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var split = require('split2')
 var to = require('to2')
+var through = require('through2')
 var pumpify = require('pumpify')
 var hyperkv = require('hyperkv')
 var sub = require('subleveldown')
@@ -12,6 +13,54 @@ function Query (opts) {
   this.log = opts.log
   this.kv = hyperkv({ db: this.db, log: this.log })
   this.archive = opts.archive
+}
+
+Query.prototype.list = function (q) {
+  var test = function () { return false }
+  if (!q) {
+    test = function () { return true }
+  } else if (q && typeof q.test === 'function') {
+    test = function (s) { return q.test(s) }
+  } else if (typeof q === 'function') {
+    test = q
+  } else {
+    var words = String(q).toLowerCase().split(/\W+/)
+    test = function (s) {
+      s = s.toLowerCase()
+      for (var i = 0; i < words.length; i++) {
+        if (s.indexOf(words[i]) < 0) return false
+      }
+      return true
+    }
+  }
+  return pumpify.obj(
+    this.kv.createReadStream({ gt: 'f/', lt: 'f/\uffff' }),
+    through.obj(function (row, enc, next) {
+      if (check(row)) next(null, row)
+      else next()
+    })
+  )
+  function check (doc) {
+    var keys = Object.keys(doc.values)
+    for (var j = 0; j < keys.length; j++) {
+      var row = doc.values[keys[j]].value
+      if (!row || !row.properties) continue
+      if (row.properties.label && test(row.properties.label)) return true
+      if (row.properties.localizedLabels) {
+        var lkeys = Object.keys(row.properties.localizedLabels)
+        var len = lkeys.length
+        for (var i = 0; i < len; i++) {
+          var lb = row.properties.localizedLabels[lkeys[i]]
+          if (Array.isArray(lb)) {
+            for (var k = 0; k < lb.length; k++) {
+              if (test(lb[k])) return true
+            }
+          } else if (test(lb)) return true
+        }
+      }
+    }
+    return false
+  }
 }
 
 Query.prototype.load = function () {
